28c0c0fc59d15d56e3ada030a79933a4411dc41f
Jay Ar Bautista <j.bautista@klab.com>
2015-12-04 07:31:33 +0800

    Unused fields

--- GoogleDriveSample/app/src/main/java/com/jayarbautista/googledrivesample/BaseDemoActivity.java
+++ GoogleDriveSample/app/src/main/java/com/jayarbautista/googledrivesample/BaseDemoActivity.java
@@ -39,30 +39,9 @@ public abstract class BaseDemoActivity extends Activity implements
     private static final String TAG = "BaseDriveActivity";
 
     /**
-     * DriveId of an existing folder to be used as a parent folder in
-     * folder operations samples.
-     */
-    public static final String EXISTING_FOLDER_ID = "0B2EEtIjPUdX6MERsWlYxN3J6RU0";
-
-    /**
-     * DriveId of an existing file to be used in file operation samples..
-     */
-    public static final String EXISTING_FILE_ID = "0ByfSjdPVs9MZTHBmMVdSeWxaNTg";
-
-    /**
-     * Extra for account name.
-     */
-    protected static final String EXTRA_ACCOUNT_NAME = "account_name";
-
-    /**
      * Request code for auto Google Play Services error resolution.
      */
     protected static final int REQUEST_CODE_RESOLUTION = 1;
-
-    /**
-     * Next available request code.
-     */
-    protected static final int NEXT_AVAILABLE_REQUEST_CODE = 2;
 
     /**
      * Google API client.
